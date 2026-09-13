b5b56aec4f18808dfdda58f7cd9881ad2c9b9311
daffadevhosting <endsae83@gmail.com>
2026-09-14 06:30:53 +0700

    update

--- sample/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$db_host	    = 'localhost';
-	$db_user        = 'tnch90_wifi';
-	$db_password	= 'wifi_ikbal2023';
-	$db_name	    = 'tnch90_wifi';
-	define('APP_URL', 'https://sendaljepit.konek.web.id​/wifi');
-	$_app_stage = 'Live';
